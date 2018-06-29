64a8b817f305565583c0cc9263dd0eac8ca699df
ackRow <hugorosen@macbook-pro-de-hugo.home>
2018-06-29 14:00:55 +0200

    Licensing

--- js/slouchy.js
+++ js/slouchy.js
@@ -1,6 +1,15 @@
 "use strict";
 "version 0.1.0";
 
+/*
+ * Copyright(C) 2018 Hugo Rosenkranz
+ *
+ * This Source Code Form is subject to the terms of the
+ * Mozilla Public License, v. 2.0. If a copy of the MPL
+ * was not distributed with this file, You can obtain one at
+ * http://mozilla.org/MPL/2.0/.
+ */
+
 const backgrndPicInterval = 5000; // 5s
 
 let STOP;
